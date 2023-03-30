031057b98b8687cebd476499144a21e3f937ea98
shmatochok-maselka <iryna.kopchak39@gmail.com>
2023-03-30 22:13:30 +0300

    feat(auth): add LogoutService and update SecurityConfiguration

--- worldoftoys/src/main/java/com/kopchak/worldoftoys/config/JwtAuthenticationFilter.java
+++ worldoftoys/src/main/java/com/kopchak/worldoftoys/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.kopchak.worldoftoys.config;
 
+import com.kopchak.worldoftoys.token.TokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,6 +22,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(
@@ -39,7 +41,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
