1bc6625486d53e217f68de05cdaee03895f9d41d
shmatochok-maselka <iryna.kopchak39@gmail.com>
2023-04-01 20:37:00 +0300

    feat(email-confirm): add EmailSenderService for sending confirmation email messages

---
+++ worldoftoys/src/main/java/com/kopchak/worldoftoys/service/EmailSenderService.java
@@ -0,0 +1,5 @@
+package com.kopchak.worldoftoys.service;
+
+public interface EmailSenderService {
+    void send(String to, String email);
+}
---
+++ worldoftoys/src/main/java/com/kopchak/worldoftoys/service/impl/EmailSenderServiceImpl.java
@@ -0,0 +1,38 @@
+package com.kopchak.worldoftoys.service.impl;
+
+import com.kopchak.worldoftoys.service.EmailSenderService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailSenderServiceImpl implements EmailSenderService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailSenderServiceImpl.class);
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("worldoftoys@gmail.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
+        }
+    }
+}
