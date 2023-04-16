35dbb30eea66c665b1f95ec854d0b08b17b990e4
shmatochok-maselka <iryna.kopchak39@gmail.com>
2023-04-16 20:41:54 +0300

    feat(product-quantity): add CartItemResponseDTO and change return of findAllProductsByUserId method in the CartItemsRepository from Product to CartItemResponseDTO

---
+++ worldoftoys/src/main/java/com/kopchak/worldoftoys/dto/CartItemResponseDTO.java
@@ -0,0 +1,18 @@
+package com.kopchak.worldoftoys.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemResponseDTO {
+    private String name;
+    private String slug;
+    private String image;
+    private BigDecimal price;
+    private Integer quantity;
+}
--- worldoftoys/src/main/java/com/kopchak/worldoftoys/repository/CartItemsRepository.java
+++ worldoftoys/src/main/java/com/kopchak/worldoftoys/repository/CartItemsRepository.java
@@ -1,5 +1,6 @@
 package com.kopchak.worldoftoys.repository;
 
+import com.kopchak.worldoftoys.dto.CartItemResponseDTO;
 import com.kopchak.worldoftoys.model.CartItemId;
 import com.kopchak.worldoftoys.model.CartItems;
 import com.kopchak.worldoftoys.model.Product;
@@ -12,8 +13,11 @@ import java.util.Set;
 
 public interface CartItemsRepository extends JpaRepository<CartItems, CartItemId> {
 
-    @Query("SELECT ci.id.product FROM CartItems ci WHERE ci.id.user.id = :userId")
-    Set<Product> findAllProductsByUserId(Integer userId);
+//    @Query("SELECT ci.id.product FROM CartItems ci WHERE ci.id.user.id = :userId")
+//    Set<Product> findAllProductsByUserId(Integer userId);
+
+    @Query("SELECT new com.kopchak.worldoftoys.dto.CartItemResponseDTO(p.name, p.slug, p.image, p.price, c.quantity) FROM CartItems c JOIN c.id.product p WHERE c.id.user.id = :userId")
+    Set<CartItemResponseDTO> findAllProductsByUserId(@Param("userId") Integer userId);
 
     @Modifying
     @Query("UPDATE CartItems c SET c.quantity = :quantity WHERE c.id = :cartItemId")
