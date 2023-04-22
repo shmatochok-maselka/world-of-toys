6817fdfb66cfbec8657bda780014c608291dde32
shmatochok-maselka <iryna.kopchak39@gmail.com>
2023-04-22 15:00:59 +0300

    feat(recipient): add OrderRecipient model and CountryCode enum for mobile numbers storage in db

---
+++ worldoftoys/src/main/java/com/kopchak/worldoftoys/model/order/CountryCode.java
@@ -0,0 +1,15 @@
+package com.kopchak.worldoftoys.model.order;
+
+public enum CountryCode {
+    UA("380"),
+    PL("48");
+    private final String countryCode;
+
+    private CountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+}
